4dacd68871ed6c0417d79d6d2ca912ce73d336fe
Aziz <azizbek.akshabakov@narxoz.kz>
2024-02-12 01:20:35 +0600

    angular material

--- task1/src/app/app.component.ts
+++ task1/src/app/app.component.ts
@@ -2,11 +2,16 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { Item } from "./item";
 import { ItemComponent } from "./item/item.component";
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, ItemComponent],
+  imports: [CommonModule, ItemComponent, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
@@ -32,10 +37,12 @@ export class AppComponent {
   }
 
   addItem(description: string) {
-    this.allItems.unshift({
-      description,
-      done: false
-    });
+    if (description) {
+      this.allItems.unshift({
+        description,
+        done: false
+      });
+    }
   }
 
   remove(item: Item) {
