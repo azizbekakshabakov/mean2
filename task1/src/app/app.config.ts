8ca266451f1dd1399889544125630cef4974011d
Aziz <azizbek.akshabakov@narxoz.kz>
2024-02-11 21:13:33 +0600

    add angular material

--- task1/src/app/app.config.ts
+++ task1/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideClientHydration(
     // withHttpTransferCacheOptions({
     //   includePostRequests: true
     // })
-  )]
+  ), provideAnimationsAsync()]
 };
