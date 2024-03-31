6b73e30a708e3a29d5f0c4e5ee9b48dd4d4def3f
Aziz <azizbek.akshabakov@narxoz.kz>
2024-03-31 14:52:45 +0500

    task8

---
+++ task8/backend/routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+const {Task, validate} = require('../schemas/task');
+const authMiddleware = require('../middleware/auth');
+const {cities} = require('../cities/cities');
+const axios = require('axios');
+
+router.post("/", authMiddleware, async (req, res) => {
+  console.log(req.body);
+  const { error } = validate(req.body);
+  if (error) res.status(400).send({ message: error.details[0].message });
+
+  const task = await Task(req.body).save();
+  res.status(201).send({ data: task, message: "Задача создана" });
+});
+
+router.get("/:cityName", async (req, res) => {
+  let latitude;
+  let longitude;
+  cities.forEach(element => {
+    if (element['name'] == req.params.cityName) {
+      latitude = element['latitude'].toFixed(4);
+      longitude = element['longitude'].toFixed(4);
+    }
+  });
+
+  axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min&forecast_days=8&timezone=auto`)
+  .then(response => {
+    // console.log(response.data);
+    res.status(200).send({ data: response.data });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+});
+
+router.get("/:id", authMiddleware, async (req, res) => {
+  const task = await Task.findOne({ _id: req.params.id });
+  res.status(200).send({ data: task });
+});
+
+router.put("/:id", authMiddleware, async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).send({ data: task, message: "Обновлена задача" });
+});
+
+router.delete("/:id", authMiddleware, async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.status(200).send({ message: "Задача удалена" });
+});
+
+module.exports = router;
---
+++ task8/front/src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { AuthInterceptor } from './services/auth-interceptor/AuthInterceptor';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoggingInterceptor } from './services/log-interceptor/AuthInterceptor';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    TaskDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatButtonToggleModule, MatDividerModule, MatListModule, MatCheckboxModule, MatGridListModule, MatToolbarModule
+  ],
+  providers: [
+    provideClientHydration(),
+    // provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
