33ad82d435d8af06bdbd6e76128b45dd47e40b20
Aziz <azizbek.akshabakov@narxoz.kz>
2024-04-01 11:15:16 +0500

    task8

--- task8/backend/routes/index.js
+++ task8/backend/routes/index.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const {Task, validate} = require('../schemas/task');
-const authMiddleware = require('../middleware/auth');
 const {cities} = require('../cities/cities');
 const axios = require('axios');
-
-router.post("/", authMiddleware, async (req, res) => {
-  console.log(req.body);
-  const { error } = validate(req.body);
-  if (error) res.status(400).send({ message: error.details[0].message });
-
-  const task = await Task(req.body).save();
-  res.status(201).send({ data: task, message: "Задача создана" });
-});
 
 router.get("/:cityName", async (req, res) => {
   let latitude;
@@ -32,23 +21,6 @@ router.get("/:cityName", async (req, res) => {
   .catch(error => {
     console.error('Error fetching data:', error);
   });
-});
-
-router.get("/:id", authMiddleware, async (req, res) => {
-  const task = await Task.findOne({ _id: req.params.id });
-  res.status(200).send({ data: task });
-});
-
-router.put("/:id", authMiddleware, async (req, res) => {
-  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.status(200).send({ data: task, message: "Обновлена задача" });
-});
-
-router.delete("/:id", authMiddleware, async (req, res) => {
-  await Task.findByIdAndDelete(req.params.id);
-  res.status(200).send({ message: "Задача удалена" });
 });
 
 module.exports = router;
