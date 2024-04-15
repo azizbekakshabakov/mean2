33236dfcf2f33b8cc0e7de413b2c0d40e2936b76
Aziz <azizbek.akshabakov@narxoz.kz>
2024-04-15 22:55:33 +0500

    task10

--- task10/front/protractor.conf.js
+++ task10/front/protractor.conf.js
@@ -10,4 +10,22 @@ exports.config = {
     //       binary: 'firefox' // Optional: Specify the path to Firefox binary
     //     }
     //   }
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          args: ['--headless', '--disable-gpu']
+        }
+      },
+    
+      // Spec patterns are relative to the current working directory when
+      // protractor is called
+    //   specs: ['path/to/specs/*.spec.js'],
+    
+      // Framework to use
+      framework: 'jasmine',
+    
+      // Options to be passed to Jasmine
+      jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+      }
 };
